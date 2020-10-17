72673864e39f95d6666093077feea76c5973febf
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-17 14:32:57 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int n;
 	cin >> n;
-	bool ok=0;
-	for(int i=1; i<=n; ++i) {
-		int a[1000], cnt=0, tmp1=i, tmp2=i;
-		while(tmp1!=0) {
-			a[cnt++]=tmp1%10;
-			tmp1/=10;
+	bool ok = false;
+	for (int i = 1; i <= n; i++) {
+		int a[1000], cnt = 0, tmp = i;
+		while (tmp != 0) {
+			a[cnt++] = tmp % 10;
+			tmp /= 10;
 		}
-		bool l=1;
-		for(int j=cnt-1; j>=0; --j)
-			if(a[j]!=4&&a[j]!=7)
-				l=0;
-		if(l)
-			if(n%tmp2==0)
-				ok=1;
+		bool lucky = true;
+		for (int j = cnt - 1; j >= 0; j--) {
+			if (a[j] != 4 && a[j] != 7) {
+				lucky = false;
+			}
+		}
+		if (lucky) {
+			if (n % i == 0) {
+				ok = true;
+			}
+		}
 	}
-	cout << (ok?"YES":"NO") << "\n";
+	cout << (ok ? "YES" : "NO") << '\n';
+	return 0;
 }
